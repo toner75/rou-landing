ac99721504cf8f7a300d87356454ef4d1d28af67
KKazlov <toner7050@gmail.com>
2021-08-17 08:51:50 +0300

    testimotion slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
-console.log('js ok');
+"use strict";
+import { tns } from "../../node_modules/tiny-slider/src/tiny-slider";
+document.addEventListener('DOMContentLoaded', () => {
+    tns({
+        container: ".my-slider",
+        controls: false,
+        items: 1,
+        speed: 1000,
+        autoplay: true,
+        autoplayHoverPause: false,
+        autoplayTimeout: 5000,
+        autoplayButtonOutput: false,
+        swipeAngle: true,
+        gutter: 15,
+        navPosition: 'bottom'
+      });
+});
